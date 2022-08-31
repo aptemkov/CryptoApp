51f6ae5df93f731c5c64a7e3ce487f93d27a207a
aptemkov <artkov37@gmail.com>
2022-08-31 11:17:27 +0300

    viewBinding added

--- src/main/java/com/example/cryptoapp/MainActivity.kt
+++ src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.cryptoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.cryptoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
