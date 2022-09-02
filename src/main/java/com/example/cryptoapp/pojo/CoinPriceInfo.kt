f2ca07860e6a6c96f009b0b6492d16821aac6943
aptemkov <artkov37@gmail.com>
2022-09-02 21:50:37 +0300

    Room added

--- src/main/java/com/example/cryptoapp/pojo/CoinPriceInfo.kt
+++ src/main/java/com/example/cryptoapp/pojo/CoinPriceInfo.kt
@@ -1,12 +1,12 @@
 package com.example.cryptoapp.pojo
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 
 import com.google.gson.annotations.SerializedName
 
-
-
-
+@Entity(tableName = "full_price_name")
 data class CoinPriceInfo (
     @SerializedName("TYPE")
     @Expose
@@ -17,7 +17,7 @@ data class CoinPriceInfo (
     @Expose
     private val market: String? = null,
 
-
+    @PrimaryKey
     @SerializedName("FROMSYMBOL")
     @Expose
     private val fromsymbol: String? = null,
