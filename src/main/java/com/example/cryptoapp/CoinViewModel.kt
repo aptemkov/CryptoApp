ad3ff14cfae6f6b4e8d7b30f29c89edea65da77e
aptemkov <artkov37@gmail.com>
2022-09-04 14:46:23 +0300

    ViewModel fully done, getting data from api finished

--- src/main/java/com/example/cryptoapp/CoinViewModel.kt
+++ src/main/java/com/example/cryptoapp/CoinViewModel.kt
@@ -1,32 +1,72 @@
 package com.example.cryptoapp
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import com.example.cryptoapp.api.ApiFactory
 import com.example.cryptoapp.database.AppDatabase
 import com.example.cryptoapp.databinding.ActivityMainBinding
+import com.example.cryptoapp.pojo.CoinPriceInfo
+import com.example.cryptoapp.pojo.CoinPriceInfoRawData
+import com.google.gson.Gson
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
-class CoinViewModel(application: Application): AndroidViewModel(application) {
+class CoinViewModel(application: Application) : AndroidViewModel(application) {
 
     private val db = AppDatabase.getInstance(application)
     private val compositeDisposable = CompositeDisposable()
+
     val priceList = db.coinPriceInfoDao().getPriceList()
 
-    fun loadData(){
-        val disposable = ApiFactory.apiService.getTopCoinsInfo()
+    fun getDetailInfo(fSym: String): LiveData<CoinPriceInfo> {
+        return db.coinPriceInfoDao().getPriceInfoAboutCoin(fSym)
+    }
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        val disposable = ApiFactory.apiService.getTopCoinsInfo(limit = 10)
+            .map { it.data?.map { it -> it.coinInfo?.name }?.joinToString(",") }
+            .flatMap { ApiFactory.apiService.getFullPriceList(fSyms = it) }
+            .map { getPriceListFromRowData(it) }
+            .delaySubscription(10, TimeUnit.SECONDS)
+            .repeat()
+            .retry()
             .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                Log.d("LOADING_DATA_TEST", it.toString())
+                db.coinPriceInfoDao().insertPriceList(it)
+                Log.d("LOADING_DATA_TEST", "Success: $it")
             }, {
-                Log.d("LOADING_DATA_TEST", it.message.orEmpty())
+                Log.d("LOADING_DATA_TEST", "Failure: ${it.message.orEmpty()}")
             })
     }
+
+    private fun getPriceListFromRowData(coinPriceInfoRawData: CoinPriceInfoRawData): List<CoinPriceInfo> {
+        val result = ArrayList<CoinPriceInfo>()
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonObject ?: return result
+        val coinKeySet = jsonObject.keySet()
+        for (coinKey in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
+    }
+
 
     override fun onCleared() {
         super.onCleared()
--- src/main/java/com/example/cryptoapp/MainActivity.kt
+++ src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.cryptoapp
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.cryptoapp.databinding.ActivityMainBinding
 
@@ -14,7 +16,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-        viewModel.loadData()
+        /*viewModel.priceList.observe(this, Observer {
+            Log.d("LOADING_DATA_TEST", "MainActivity, Success: $it")
+        })*/
 
+        viewModel.getDetailInfo("BTC").observe(this, Observer {
+            Log.d("LOADING_DATA_TEST", "MainActivity, Success: $it")
+        })
     }
 }
